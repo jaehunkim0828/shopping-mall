3bdd14a5027054f764c09448482a2374cd628920
JaeHun Kim <kkaa81@naver.com>
2022-04-12 19:30:12 +0900

    feat: testing component

--- pages/makeComponents.tsx
+++ pages/makeComponents.tsx
@@ -1,9 +1,14 @@
+import HomeSlide from "../components/HomeSlide";
 import SlideImage from "../components/SlideImage";
+import Thumb from "../components/Thumb";
+import { thumb} from '../fakeData';
 
 export default function makeComponents() {
     return (
         <div style={{display: 'flex', justifyContent: 'center'}}>
-            <SlideImage size={400}/>
+            { thumb.map((data, i) => {
+                return <Thumb data={data} size={400} />
+            })}
         </div>
     )
 }
