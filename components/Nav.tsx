f9a84ba1f895d4fb04899b38aa99d53ef49d5e5f
JaeHun Kim <kkaa81@naver.com>
2022-04-16 19:26:30 +0900

    feat: add login-state

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,23 +1,38 @@
 import Image from 'next/image';
 import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
 
 import style from '../styles/nav.module.scss';
 import logo from '../public/images/logo.jpg'
 import NavOption from './NavOption';
-import { fakeNavOption, fakeNavProfile } from '../fakeData';
+import { fakeNavOption, fakeNavProfile, fakeNavLogin } from '../fakeData';
 import NavProfile from './NavProfile';
+import { useEffect, useState } from 'react';
+import { RootState } from '../store/reducers';
 
 export default function Nav() {
     const router = useRouter();
+    const { state } = useSelector((state: RootState) => state.loginState);
+
+    
+    const [navProfile, setNavProfile] = useState<{name: string, path?: string}[]>(fakeNavLogin);
 
     const goMain = () => {
         router.push('/');
     }
+
+    useEffect(() => {
+        if (window.localStorage.getItem('token')) {
+            setNavProfile(fakeNavProfile);
+            return;
+        }
+        setNavProfile(fakeNavLogin);
+    }, [state])
     
     return (
         <div className={style.navContainer}>
             <div className={style.navLoginSection}>
-                {fakeNavProfile.map((option, i) => <NavProfile key={`nav profile index ${i}`} name={option.name} path={option.path}/>)}
+                {navProfile.map((option, i) => <NavProfile key={`nav profile index ${i}`} name={option.name} path={option.path}/>)}
             </div>
             <div className={style.navLogoContainer}>
                 <div onClick={goMain}>
