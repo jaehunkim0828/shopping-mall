b53c1887c0fc88ad8948ffe35463b3032b282ee3
JaeHun Kim <kkaa81@naver.com>
2022-04-16 19:25:59 +0900

    feat: redux and kakao script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
+import { useEffect } from 'react'
+
+import { wrapper } from '../store';
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    try {
+      if (!window.Kakao.isInitialized() && window.Kakao) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT);
+      }
+    } catch(e) { console.log(e)}
+
+  }, [])
   return (
     <div>
       <Nav />
@@ -11,4 +29,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
