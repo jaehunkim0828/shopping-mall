e1e4fbf1f57cde7def4716dbe6db56700ca4a67f
JaeHun Kim <kkaa81@naver.com>
2022-04-19 22:22:34 +0900

    fix: move kakao button

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,35 +5,30 @@ import { useEffect } from 'react';
 
 import { RootState } from '../store/reducers';
 import style from '../styles/login.module.scss';
-import kakao from '../public/images/kakao_login_medium_narrow.png';
 import { loginAct } from '../store/action/loginState';
+import LoginGoogle from '../components/LoginGoogle';
+import LoginKakao from '../components/LoginKakao';
+import LoginNaver from '../components/LoginNaver';
+
+
 
 export default function login() {
     const router = useRouter();
     const dispatch = useDispatch();
     const { state } = useSelector((state: RootState) => state.loginState);
 
-    const loginFormWithKakao = () => {
-        window.Kakao.Auth.login({
-            success(authObj: any) {
-                console.log(authObj);
-                window.localStorage.setItem('token', authObj.access_token);
-                router.push('/');
-                dispatch(loginAct());
-            },
-            fail(err: any) {
-                console.log(err);
-            }
-        })
-    }
-
     useEffect(() => {
-        if (window.localStorage.getItem('token')) router.push('/');
+        if (window.location.href.includes('access_token')) {
+            window.localStorage.setItem('token', window.location.href.split('=')[1].split('&')[0]?? 'none');
+            dispatch(loginAct());
+            router.push('/');
+        };
     }, [state])
+
     return (
         <div className={style.loginContainer}>
             <div className={style.loginForm}>
-                <div>회원 로그인</div>
+                <div className={style.desc}>회원 로그인</div>
                 <form>
                     <div>
                         <div className={style.loginInput}>
@@ -47,16 +42,11 @@ export default function login() {
                     </div>
                     <button>로그인</button>
                 </form>
-                <div>SNS 간편 로그인</div>
+                <div className={style.desc} >SNS 간편 로그인</div>
                 <div className={style.snsContainer}>
-                    <button>네이버</button>
-                    <button>구글</button>
-                    <button onClick={loginFormWithKakao}>
-                        <Image 
-                            src={kakao}
-                            alt='카카오 로그인 버튼'
-                        />
-                    </button>
+                    <LoginNaver />
+                    <LoginGoogle />
+                    <LoginKakao />
                 </div>
                 <div>
                     <div>
